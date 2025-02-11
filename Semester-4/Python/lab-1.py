1284c887da90089c3298ae4fc9d6c2bafa9424fe
GeorgiTsukanov <georgicukanov@gmail.com>
2025-02-11 12:04:10 +0300

    Create lab-1.py

---
+++ Semester-4/Python/lab-1.py
@@ -0,0 +1,27 @@
+# 1
+'''
+b1 = int(input())
+q = int(input())
+n = int(input())
+print(b1*q**(n-1))
+'''
+# 4
+'''
+a = int(input())
+print(a + 2 - a%2)
+print(a + a%2 + (int(not(a%2))*2))
+'''
+
+#6
+'''
+m = int(input())
+print(m//2 + m%2)
+'''
+
+#8
+'''
+a = int(input())
+b = int(input())
+for i in range(a - a%2 + 1, b + b%2, 2):
+    print(i)
+'''
